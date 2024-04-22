c59a0e21b7bc896ba557586d64f79f50e2ecede1
rsanthosh1996 <153453429+rsanthosh1996@users.noreply.github.com>
2024-04-22 17:33:22 +0530

    santosh.java

--- santosh.java
+++ santosh.java
@@ -174,3 +174,203 @@ in java same name allowed in function with diff parameters.
 const overloading - defualt ,parametraized 
 operator -- + add or concatenation.
 
+ -- constructor overloading 
+class demo {
+    demo() {
+        System.out.println("hello");
+    }
+
+    demo(int a) {
+        System.out.println("a=" + a);
+    }
+}
+
+public class demo1 {
+    public static void main(String args[]) {
+        demo a1 = new demo();
+        demo a2 = new demo(3);
+    }
+}
+
+/// methods overloader 
+
+
+class a {
+    void display(){
+        System.out.println("err");
+    }
+    void display(int b){
+        System.out.println(+b);
+    }
+}
+public class c {
+    public static void main (String args[]){
+        a str=new a();
+        str.display();
+        str.display(12);
+    }
+}
+
+//Operator overloader 
+class demo{
+    int a =13;
+    int b =12;
+    public  static void main (String args[]){
+        demo obj = new demo();
+        System.out.println("a+b="+(obj.a+obj.b)); // for sum use braket.
+    }
+}  
+
+class demo{
+    int a =13;
+    int b =12;
+    public  static void main (String args[]){
+        demo obj = new demo();
+        System.out.println("a+b="+obj.a+obj.b);// for concatnate dont use brackets 
+    }
+}  
+
+
+///This key word . -- when we have instance and local variable have same name ,local variable will be given preference .
+so we can use this key where instance variable will be printed.
+
+class demo{
+    int a = 10;
+    void display(){
+        int a = 20;
+        System.out.println(this.a);
+    }
+    public static void main(String args[]){
+        demo obj = new demo();
+        obj.display();
+    }
+}
+
+///Inheritance 
+parent cant access method and variables of child class but reverse can happen 
+use extends key word 
+types --  single,multiple ,hierarcchy inheritance 
+single-- one base class and one child class 
+multiple -- base (parent) --- derived (P) --- drived (c)
+hierarchy --- base -- derived\derived.
+
+multiple?? and hybrid inheritance cant be achieved in java.but can be achieved by interfaces.
+
+//Single inheritance code 
+
+class parent {
+    int a = 10;
+    void display(){
+        System.out.println("hi");
+    }
+    
+}
+
+class child extends parent{
+    int b= 30;
+    void wow(){
+        System.out.println("ro");
+    }
+}
+public class jam{
+    public static void main(String args[]){
+        child obj= new child();
+        System.out.println(obj.b);
+        System.out.println(obj.a);
+        obj.display();
+        obj.wow();
+        
+    }
+}
+
+// alternate code.
+
+class parent {
+    int a = 10;
+    void display(){
+        System.out.println("hi");
+    }
+    
+}
+
+public class child extends parent{
+    int b= 30;
+    void wow(){
+        
+        System.out.println("ro");
+    }
+    public static void main(String args[]){
+        child obj= new child();
+        System.out.println(obj.b);
+        System.out.println(obj.a);
+        obj.display();
+        obj.wow();
+}
+}
+
+// multi level inheritance
+
+    class Parent {
+    int a = 10;
+    void display() {
+        System.out.println("hi");
+    }
+}
+
+ class Child extends Parent {
+    int b = 20;
+
+    void wow() {
+        System.out.println("ji");
+    }
+}
+
+  public class GChild extends Child {
+    int c = 30;
+    void fun() {
+        System.out.println("yo");
+    }
+    public static void main(String[] args) {
+        GChild v = new GChild();
+        v.wow();
+        v.fun();
+        v.display();
+        System.out.println(v.a);
+        System.out.println(v.b);
+        System.out.println(v.c);
+    }
+} 
+
+//hirarchical inheritance
+class parent {
+    int a=10;
+    void display(){
+        System.out.println("hi");
+    }
+}
+
+class child1 extends parent {
+    int b=100;
+    void wow(){
+        System.out.println("ji");
+    }
+}
+    
+    class child2 extends parent {
+    int c=101;
+    void woe(){
+        System.out.println("ki");
+    }
+    }
+    
+    public class hierarchy {
+    public static void main(String args[]){
+        child1 obj1=new child1();
+        obj1.display();
+        System.out.println(obj1.a);
+        child2 obj2 =new child2();
+        obj2.display();
+        System.out.println(obj2.c);
+    }
+}
+
